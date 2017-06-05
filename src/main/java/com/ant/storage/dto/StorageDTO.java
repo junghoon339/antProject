2f234866465c1185b25720a17d8e9fb63975904c
KOSTA-HP\KOSTA <jsonjson0627@gmail.com>
2017-06-05 15:34:55 +0900

    dto 내용 추가

--- src/main/java/com/ant/storage/dto/StorageDTO.java
+++ src/main/java/com/ant/storage/dto/StorageDTO.java
@@ -1,5 +1,59 @@
 package com.ant.storage.dto;
 
 public class StorageDTO {
-
+	private int storageNo;
+	private int userNo;
+	private int projectNo;
+	private String fileName;
+	private String filePath;
+	private int fileSize;
+	private int readNum;
+	
+	public StorageDTO(){
+		
+	}
+	public int getStorageNo() {
+		return storageNo;
+	}
+	public void setStorageNo(int storageNo) {
+		this.storageNo = storageNo;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public int getProjectNo() {
+		return projectNo;
+	}
+	public void setProjectNo(int projectNo) {
+		this.projectNo = projectNo;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public int getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(int fileSize) {
+		this.fileSize = fileSize;
+	}
+	public int getReadNum() {
+		return readNum;
+	}
+	public void setReadNum(int readNum) {
+		this.readNum = readNum;
+	}
+	
+	
 }
