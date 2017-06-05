a85bc95b528aadca8ae993e0568190daa449c21e
KOSTA-HP\KOSTA <hyj818@gmail.com>
2017-06-05 14:41:12 +0900

    project 패키지 초기 설정

---
+++ src/main/java/com/ant/project/dto/ProjectDTO.java
@@ -0,0 +1,15 @@
+package com.ant.project.dto;
+
+public class ProjectDTO {
+	private String projectName;
+	private String projectStartDate;
+	private String projectEndDate;
+	private String projectSubject;
+	private String projectteacher;
+	private int projectState;
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/ant/project/dto/ProjectUserDTO.java
@@ -0,0 +1,5 @@
+package com.ant.project.dto;
+
+public class ProjectUserDTO {
+
+}
