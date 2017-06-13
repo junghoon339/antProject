764d40212f972f314c58f23f7df756e9d310a141
DESKTOP-CDVCE6L\나용호 <gh6819@gmail.com>
2017-06-13 18:08:45 +0900

    vote 기능구현(★★★☆☆, 3/5) 및 vote관련 Selector table, DTO 추가됨 -용호 2017-06-13

--- src/main/java/com/ant/vote/dto/VoteDetailDTO.java
+++ src/main/java/com/ant/vote/dto/VoteDetailDTO.java
@@ -1,20 +1,30 @@
 package com.ant.vote.dto;
 
+import java.util.List;
+
 public class VoteDetailDTO {
 
 	private int voteDetailNo;
 	private int voteNo;
-	private int userNo;
 	private String voteDetailColumn;
 
+	private List<VoteSelectorDTO> selectors;
+	
 	public VoteDetailDTO(){}
-	public VoteDetailDTO(int voteDetailNo, int voteNo, int userNo, String voteDetailColumn) {
+	public VoteDetailDTO(int voteDetailNo, int voteNo, String voteDetailColumn) {
 		this.voteDetailNo = voteDetailNo;
 		this.voteNo = voteNo;
-		this.userNo = userNo;
 		this.voteDetailColumn = voteDetailColumn;
 	}
 
+	public List<VoteSelectorDTO> getSelectors() {
+		return selectors;
+	}
+	
+	public void setSelectors(List<VoteSelectorDTO> selectors) {
+		this.selectors = selectors;
+	}
+	
 	public int getVoteDetailNo() {
 		return voteDetailNo;
 	}
@@ -29,14 +39,6 @@ public class VoteDetailDTO {
 
 	public void setVoteNo(int voteNo) {
 		this.voteNo = voteNo;
-	}
-
-	public int getUserNo() {
-		return userNo;
-	}
-
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
 	}
 
 	public String getVoteDetailColumn() {
